35e82e4c945c0057d71cd4aa71041877c7c1ec89
Kento75 <kento2github@gmail.com>
2019-02-08 06:12:25 +0900

    Add base app

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import propTypes from 'prop-types';
+import React from 'react'
+import propTypes from 'prop-types'
 
-function Button({ text, onClick }) {
-  return (
+const Button = ({ text, onClick }) => (
     <button onClick={onClick} type="button">
       {text}
     </button>
-  );
-}
+)
 
 Button.propTypes = {
   text: propTypes.string.isRequired,
   onClick: propTypes.func,
-};
+}
 
 Button.defaultProps = {
   onClick: null,
-};
+}
 
-export default Button;
+export default Button
