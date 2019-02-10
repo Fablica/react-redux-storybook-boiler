1b0b4c95e8f7a19b4e10e8265062cb4f633eb31b
Kento75 <kento2github@gmail.com>
2019-02-10 21:17:38 +0900

    Add simple base app

--- src/components/Button/index.stories.js
+++ src/components/Button/index.stories.js
@@ -2,7 +2,7 @@ import React from "react"
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
 import { text } from "@storybook/addon-knobs"
-import Button from "."
+import Button from "./Button"
 
 const stories = storiesOf("Button", module)
 
